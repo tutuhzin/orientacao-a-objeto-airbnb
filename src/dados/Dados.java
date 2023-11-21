bbb9c52bd16b01b49b91e8e9339273e08bb73c30
trench0511 <149620306+trench0511@users.noreply.github.com>
2023-11-21 09:46:55 -0300

    Update Dados.java
    criei o listar imoveis

--- src/dados/Dados.java
+++ src/dados/Dados.java
@@ -15,46 +15,48 @@ public class Dados {
 
     // Construtor
     public Dados() {
-        imoveis = new Imovel[100];
+        imoveis = new Imovel[100];  
         
-        tiposImovel = new String[] {"Casa", "Apartamento", "Chácara", "Chalé", "Pousada", "Kitnet"};
+        tiposImovel = new String[] {" Casa", " Apartamento", " Chácara", " Chalé", " Pousada", " Kitnet"};
 
         nomes = new String []{
-            "Alan", "Luiza", "Helena", "Beatriz", "Mariana", "Livia", "Ana Clara", "Rafaela", "Yasmin",
-            "Gabriela", "Melissa", "Esther", "Isis", "Maria", "Maria Fernanda", "Catarina", "Alicia", 
-            "Maria Alice", "Amanda", "Alicia", "Valentina", "Ana", "Alana", "Isis", "Clara", "Gabriela", 
-            "Yasmin", "Larissa", "Ester", "Bruna", "Eduarda", "Mariana", "Laila", "Maisa", "Brenda", 
-            "Isabel", "Marina", "Estela", "Lia", "Micaela", "Ariana", "Debora", "Daniela", "Paloma",
-            "Isa", "Karen", "Sabrina", "Ingrid", "Carol", "Clarissa", "Alessandra", "Adriana",
-            "Natalia", "Flor", "Bernardo", "Rafael", "Enzo", "Lorenzo", "Guilherme", "Samuel",
-            "Eduardo", "Joao", "Francisco", "Antonio", "Davi Lucas", "Enrico", "Artur", "Bernardo", 
-            "Miguel", "Enzo", "Lorenzo", "Theo", "Gabriel", "Henrique", "Lucas", "Rafael", "Daniel", 
-            "Eduardo", "Victor", "Andre", "Levi", "Aarao", "Vicente", "Vinicius", "Lourenzo", "Francisco", 
-            "Valentim", "Angelo", "Leandro", "Marcelo", "Alvaro", "Frederico", "Abel", "Santiago", 
-            "Carlos", "Cesar", "Fabio", "Muriel", "Adriano", "Cristiano", "Caetano", "Sophia", 
-            "Maria Luiza", "Mariana", "Lara", "Gabriela", "Isabelly", "Cecilia", "Esther", "Marina", 
-            "Vitoria", "Isis", "Ana", "Laura", "Lara", "Cecilia", "Luisa", "Melissa", "Livia", "Ana", 
-            "Lorena", "Isadora", "Yasmin", "Barbara", "Abigail", "Larissa", "Camila", "Sara", "Vitoria", 
-            "Amanda", "Melinda", "Agnes", "Analice", "Diana", "Emanuela", "Anita", "Karen", "Sabrina", 
-            "Belinda", "Ingrid", "Alessandra", "Raquel", "Miguel", "Davi", "Lucas", "Heitor", "Rafael", 
-            "Enzo", "Nicolas", "Guilherme", "Samuel", "Theo", "Gustavo", "Murilo", "Vitor", "Isaac", 
-            "Caio", "Vicente", "Francisco", "Thomas", "Artur", "Miguel", "Theo", "Guilherme", "Gustavo", 
-            "Caio", "David", "Daniel", "Joaquim", "Eduardo", "Samuel", "Leonardo", "Bruno", "Hugo", 
-            "Igor", "Levi", "Kevin", "Luis", "Diego", "Rodrigo", "Afonso", "Danilo", "Filipe", "Antonio", 
-            "Valentim", "Angelo", "Octavio", "Leandro", "Alvaro", "Frederico", "Ricardo", "Aquiles", 
-            "Paulo", "Carlos", "Wilian", "Ezequiel", "Jonas", "Adriano", "Mauricio", "Caetano"
-        };
+        		" Alan", " Luiza", " Helena", " Beatriz", " Mariana", " Livia", " Ana Clara", " Rafaela", " Yasmin",
+        		" Gabriela", " Melissa", " Esther", " Isis", " Maria", " Maria Fernanda", " Catarina", " Alicia", 
+        		" Maria Alice", " Amanda", " Alicia", " Valentina", " Ana", " Alana", " Isis", " Clara", " Gabriela", 
+        		" Yasmin", " Larissa", " Ester", " Bruna", " Eduarda", " Mariana", " Laila", " Maisa", " Brenda", 
+        		" Isabel", " Marina", " Estela", " Lia", " Micaela", " Ariana", " Debora", " Daniela", " Paloma",
+        		" Isa", " Karen", " Sabrina", " Ingrid", " Carol", " Clarissa", " Alessandra", " Adriana",
+        		" Natalia", " Flor", " Bernardo", " Rafael", " Enzo", " Lorenzo", " Guilherme", " Samuel",
+        		" Eduardo", " Joao", " Francisco", " Antonio", " Davi Lucas", " Enrico", " Artur", " Bernardo", 
+        		" Miguel", " Enzo", " Lorenzo", " Theo", " Gabriel", " Henrique", " Lucas", " Rafael", " Daniel", 
+        		" Eduardo", " Victor", " Andre", " Levi", " Aarao", " Vicente", " Vinicius", " Lourenzo", " Francisco", 
+        		" Valentim", " Angelo", " Leandro", " Marcelo", " Alvaro", " Frederico", " Abel", " Santiago", 
+        		" Carlos", " Cesar", " Fabio", " Muriel", " Adriano", " Cristiano", " Caetano", " Sophia", 
+        		" Maria Luiza", " Mariana", " Lara", " Gabriela", " Isabelly", " Cecilia", " Esther", " Marina", 
+        		" Vitoria", " Isis", " Ana", " Laura", " Lara", " Cecilia", " Luisa", " Melissa", " Livia", " Ana", 
+        		" Lorena", " Isadora", " Yasmin", " Barbara", " Abigail", " Larissa", " Camila", " Sara", " Vitoria", 
+        		" Amanda", " Melinda", " Agnes", " Analice", " Diana", " Emanuela", " Anita", " Karen", " Sabrina", 
+        		" Belinda", " Ingrid", " Alessandra", " Raquel", " Miguel", " Davi", " Lucas", " Heitor", " Rafael", 
+        		" Enzo", " Nicolas", " Guilherme", " Samuel", " Theo", " Gustavo", " Murilo", " Vitor", " Isaac", 
+        		" Caio", " Vicente", " Francisco", " Thomas", " Artur", " Miguel", " Theo", " Guilherme", " Gustavo", 
+        		" Caio", " David", " Daniel", " Joaquim", " Eduardo", " Samuel", " Leonardo", " Bruno", " Hugo", 
+        		" Igor", " Levi", " Kevin", " Luis", " Diego", " Rodrigo", " Afonso", " Danilo", " Filipe", " Antonio", 
+        		" Valentim", " Angelo", " Octavio", " Leandro", " Alvaro", " Frederico", " Ricardo", " Aquiles", 
+        		" Paulo", " Carlos", " Wilian", " Ezequiel", " Jonas", " Adriano", " Mauricio", " Caetano"
 
+        };
+ 
         sobrenomes = new String[] {
-            "Silva", "Santos", "Oliveira", "Souza", "Rodrigues", "Ferreira", "Almeida", 
-            "Pereira", "Lima", "Gomes","Costa", "Ribeiro", "Martins", "Jesus", "Pinto", "Mendes", "Carvalho",
-            "Ramos", "Teixeira", "Andrade","Melo", "Barros", "Franco", "Tavares", "Rocha", "Campos", 
-            "Figueiredo", "Barbosa", "Morais", "Cavalcanti","Dias", "Castro", "Camargo", "Padilha", 
-            "Monteiro", "Senhorinho", "Arruda", "Bueno" ,"Vasconcelos", "Bittencourt", "Sampaio","Miranda", 
-            "Azevedo", "Brito", "Correia", "Batista", "Marques", "Guedes", "Moraes", "Peixoto", "Maia",
-            "Barreto", "Soares", "Pessoa", "Rangel", "Aquino", "Araujo", "Nascimento", "Lemos", "Salazar",
-            "Portela","Viana", "Macedo", "Borges", "Carneiro", "Cruz", "Brandão", "Pires", "Carmo", "Neves", 
-            "Prado","Silveira", "Queiroz", "Aguiar", "Siqueira", "Nunes"
+        		" Silva", " Santos", " Oliveira", " Souza", " Rodrigues", " Ferreira", " Almeida", 
+        		" Pereira", " Lima", " Gomes"," Costa", " Ribeiro", " Martins", " Jesus", " Pinto", " Mendes", " Carvalho",
+        		" Ramos", " Teixeira", " Andrade"," Melo", " Barros", " Franco", " Tavares", " Rocha", " Campos", 
+        		" Figueiredo", " Barbosa", " Morais", " Cavalcanti"," Dias", " Castro", " Camargo", " Padilha", 
+        		" Monteiro", " Senhorinho", " Arruda", " Bueno" ," Vasconcelos", " Bittencourt", " Sampaio"," Miranda", 
+        		" Azevedo", " Brito", " Correia", " Batista", " Marques", " Guedes", " Moraes", " Peixoto", " Maia",
+        		" Barreto", " Soares", " Pessoa", " Rangel", " Aquino", " Araujo", " Nascimento", " Lemos", " Salazar",
+        		" Portela"," Viana", " Macedo", " Borges", " Carneiro", " Cruz", " Brandão", " Pires", " Carmo", " Neves", 
+        		" Prado"," Silveira", " Queiroz", " Aguiar", " Siqueira", " Nunes"
+
         };
     }
 
@@ -97,6 +99,16 @@ public class Dados {
 
     public void setnImoveis(int nImoveis) {
         this.nImoveis = nImoveis;
+    }
+    
+    // Vai preencher os imoveis e proprietarios
+    public void preencherDados ()
+    {
+    	for (int i = 0; i <10; i++)
+    	{
+    		gerarProprietario ();
+    	}
+    	//System.out.println("Dados preenchidos com sucesso!");
     }
 
     // Geradores de Dados Aleatórios
@@ -147,7 +159,7 @@ public class Dados {
         Proprietario novoProprietario = new Proprietario(nomeCompleto, email, telefoneAleatorio);
         
         novoProprietario.addImovel(gerarImovel());
-
+        //System.out.println("Proprietario adicionado com sucesso!");
         return novoProprietario;
     }
     
@@ -155,27 +167,29 @@ public class Dados {
     public Imovel gerarImovel() {
         Random random = new Random();
     
-        String descricao = "Imóvel Aleatório";
+        String descricao = "Imóvel n° " + nImoveis;
         String tipoImovel = getTiposImovel()[random.nextInt(tiposImovel.length)];
-        int qntQuartos = random.nextInt(5) + 1;
+        int qntQuartos = + random.nextInt(5) + 1;
         int qntCamas = random.nextInt(5) + 1;
         int qntBanheiros = random.nextInt(3) + 1;
         
         Imovel novoimovel = new Imovel(descricao, tipoImovel, qntQuartos, qntCamas, qntBanheiros);
         adicionarImovel(novoimovel);
-
+     
         return novoimovel;
     }
     
-    
+ 
+
+	
     // Adicionar um objeto imovel no array de imoveis
     public void adicionarImovel(Imovel novoImovel) {
         if (nImoveis < imoveis.length) {
             imoveis[nImoveis] = novoImovel;
             nImoveis++;
-            System.out.println("Imovel adicionado com sucesso!");
+            //System.out.println("Imovel adicionado com sucesso!");
         } else {
-            System.out.println("A capacidade maxima de anuncios foi atingida. Nao eh possivel adicionar mais imoveis.");
+            //System.out.println("A capacidade maxima de anuncios foi atingida. Nao eh possivel adicionar mais imoveis.");
         }
     }
     
@@ -203,6 +217,18 @@ public class Dados {
         }
     }
     
+    public void listarImoveis ()
+    {
+    	for (int i = 1; i < nImoveis; i++) {
+    		
+            System.out.println(imoveis[i].getDescricao() + " = Tipo:" + imoveis[i].getTipoImovel() + ","
+            		+ " Quartos:" + imoveis[i].getQntQuartos() + ","
+            		+ " Camas:" + imoveis[i].getQntCamas() + ","
+            		+ " Banheiros:" + imoveis[i].getQntBanheiros() + ",");
+        }
+    }
+    
+    /*
     // Busca um objeto imovel especifico no array de imoveis
     public void buscarImovel(Imovel imovelParaBuscar) {
     	boolean encontrado = false;
@@ -216,6 +242,7 @@ public class Dados {
         	System.out.println("Imovel nao encontrado.");
         }
     }
+    */
 
     @Override
     public String toString() {
